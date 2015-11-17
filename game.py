96b96286ed96937e0a2e5b901720a75eb757608b
Alexis Lameire <alexis.lameire@gmail.com>
2015-11-17 22:24:07 +0100

    add output on party start and allow people to join a party

--- game.py
+++ game.py
@@ -11,12 +11,30 @@ class CAHGame(object):
         self.channel = channel
         self.serverData = serverData
         self.players = []
+        self.state = 'NOT_RUNNING'
+        dispatch.appendCmd('join', self.joinCmd)
+
+    def _say(self, msg):
+        self.serverData.privmsg(self.channel, msg)
 
     def _addPlayer(self, nick):
         logger.info('add a new player : ' + nick) 
         self.players.append(Player(nick))
+        self._say('{0} join the party'.format(nick))
 
+    def joinCmd(self, serverData, channel, user, args):
+        logger.info('join command called')
+        if self.state == 'NOT_RUNNING':
+            logger.error('no party currently running')
+            return
+        for i in self.players:
+            if i.nick == user:
+                logger.warning('user already registered')
+                return
+        self._addPlayer(user)
+ 
     def start(self, nick):
         logger.info('new game on channel ' + self.channel)
+        self._say('Begining a new party, wait 1 min for players')
         self._addPlayer(nick)
-
+        self.state = 'WAIT_PEOPLE'
