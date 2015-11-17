729aa3c2b5dfe2dfa4c5c9cbdc9ff6a135be6f18
Alexis Lameire <alexis.lameire@gmail.com>
2015-11-17 22:52:40 +0100

    add timeout on waiting people

--- game.py
+++ game.py
@@ -1,5 +1,5 @@
 import logging
-
+from threading import RLock, Timer
 logger = logging.getLogger(__name__)
 
 class Player(object):
@@ -12,6 +12,7 @@ class CAHGame(object):
         self.serverData = serverData
         self.players = []
         self.state = 'NOT_RUNNING'
+        self.lockState = RLock()
         dispatch.appendCmd('join', self.joinCmd)
 
     def _say(self, msg):
@@ -24,17 +25,31 @@ class CAHGame(object):
 
     def joinCmd(self, serverData, channel, user, args):
         logger.info('join command called')
-        if self.state == 'NOT_RUNNING':
-            logger.error('no party currently running')
-            return
-        for i in self.players:
-            if i.nick == user:
-                logger.warning('user already registered')
+        with self.lockState:
+            if self.state == 'NOT_RUNNING':
+                logger.error('no party currently running')
                 return
-        self._addPlayer(user)
+            for i in self.players:
+                if i.nick == user:
+                    logger.warning('user already registered')
+                    return
+            self._addPlayer(user)
  
+    def _endWaitPeople(self):
+        logger.info('end of timeout, check if enough people to play')
+        with self.lockState:
+            if len(self.players) < 3:
+                self.state = 'NOT_RUNNING'
+                self._say('You need to be 3 to play, only {0} people here, stop game'.format(len(self.players)))
+                return
+            self.state = 'ROUND_START'
+        logger.info('game start')
+
+
     def start(self, nick):
         logger.info('new game on channel ' + self.channel)
         self._say('Begining a new party, wait 1 min for players')
         self._addPlayer(nick)
-        self.state = 'WAIT_PEOPLE'
+        with self.lockState:
+            Timer(10, self._endWaitPeople).start()
+            self.state = 'WAIT_PEOPLE'
