a59c1d77688553ce4e2a5abeb75332ad4985e852
Alexis Lameire <alexis.lameire@gmail.com>
2015-11-20 23:28:25 +0100

    add czar management, reorganize code for more clarety on workflow and add the pick command

--- game.py
+++ game.py
@@ -6,6 +6,7 @@ class Player(object):
     def __init__(self, nick):
         self.nick = nick
         self.heap = []
+        self.score = 0
 
     def addCard(self, card):
         self.heap.append(card)
@@ -18,27 +19,66 @@ class Player(object):
             elem += 1
             msg += '[{}] {} '.format(elem, card)
         serverData.privmsg(self.nick, msg)
+    
+    def removeCards(self, cards):
+        cards.sort()
+        for index, values in cards:
+           self.heap.remove(value - index)
 
-class CAHGame(object):
-    def __init__(self, dispatch, channel, serverData, blackCardStack, whiteCardStack):
-        self.channel = channel
+class PlayedCards(object):
+    def __init__(self):
+        self.heap = []
+
+    def append(self, nick, cards):
+        self.heap.append({ 'nick' : nick, 'cards' : cards})
+
+    def shuffle(self):
+        shuffle(self.heap)
+
+    def search(self, nick):
+        for i in self.heap:
+            if i['nick'] == nick:
+                return True
+        return False
+
+class CAHGameUtils(object):
+    def __init__(self, serverData, channel):
         self.serverData = serverData
+        self.channel = channel
         self.players = []
-        self.state = 'NOT_RUNNING'
-        self.lockState = RLock()
-        self.whiteCardStack = whiteCardStack
-        self.blackCardStack = blackCardStack
-        dispatch.appendCmd('join', self.joinCmd)
+
+    def _sayScore(msg):
+        scoreMsg = 'score : '
+        for player in player:
+            scorePlayerMsg = '{} : {},'.format(player.nick, player.score) 
+            scoreMsg + scorePlayerMsg
+        scoreMsg = scoreMsg[:-1]
+        self._say(scoreMsg)
 
     def _say(self, msg):
         self.serverData.privmsg(self.channel, msg)
+
+    def _privateSay(self, nick, msg):
+        self.serverData.privmsg(nick, msg)
 
     def _addPlayer(self, nick):
         logger.info('add a new player : ' + nick) 
         self.players.append(Player(nick))
         self._say('{0} join the party'.format(nick))
 
+
+class CAHGame(CAHGameUtils):
+    def __init__(self, dispatch, channel, serverData, blackCardStack, whiteCardStack):
+        super(CAHGame, self).__init__(serverData, channel)
+        self.state = 'NOT_RUNNING'
+        self.lockState = RLock()
+        self.whiteCardStack = whiteCardStack
+        self.blackCardStack = blackCardStack
+        self.czar = -1
+        dispatch.appendCmd('join', self.joinCmd)
+
     def joinCmd(self, serverData, channel, user, args):
+        ''' 1) join a party '''
         logger.info('join command called')
         with self.lockState:
             if self.state == 'NOT_RUNNING':
@@ -51,6 +91,7 @@ class CAHGame(object):
             self._addPlayer(user)
  
     def _endWaitPeople(self):
+        ''' 2) wait all people '''
         logger.info('end of timeout, check if enough people to play')
         with self.lockState:
             #if len(self.players) < 3:
@@ -63,19 +104,96 @@ class CAHGame(object):
         self._beginTurn()
 
     def _giveInitialTurn(self):
+        ''' 3) give initial hand'''
         logger.debug('give initial hand for each player')
         for player in self.players:
             for cardNumber in range(0,9):
                 player.addCard(self.whiteCardStack.pick())
 
     def _beginTurn(self):
+        ''' 4) select the czar, pick black card, pick white card ans wait 
+        for white people to play
+        '''
         logger.debug('begin a new turn')
-        blackCard = self.blackCardStack.pick()
-        self._say("sentance is : " + blackCard.printEmpty())
+        self.playedCards = PlayedCards()
+        self.czar = (self.czar + 1) % len(self.players)
+        self._say("{} is the czar".format(self.players[self.czar].nick))
+        self.currentBlackCard = self.blackCardStack.pick()
+        self._say("sentance is : " + self.currentBlackCard.printEmpty())
         for player in self.players:
+            if player == self.players[self.czar]:
+                continue
             for cardNumber in range(0, blackCard.pick):
                 player.addCard(self.whiteCardStack.pick())
             player.sayGame(self.serverData)
+
+        with self.lockState:
+            self.state = 'WAIT_BLACK'
+        self.currenTimer = Timer(60, self._endWaitWhiteCard).start()
+
+
+    def _playWhiteCards(self, serverData, channel, user, args):
+        ''' 5) play white card '''
+        logger.info('{} is playing white cards {}', user, args)
+        logger.debug('search for player')
+        player = None
+        for checkedPlayer in self.players:
+            if checkedPlayer.nick == user:
+                player = checkedPlayer
+                break
+        if player == None:
+            logger.debug('not a player is currently play')
+            return
+        if self.playedCards.search(user):
+            logger.debug('player have already played')
+            return
+        if len(args) != self.currentBlackCard.pick:
+            self.warning('played {} white card, need {}'.format(len(args), self.currentBlackCard.pick))
+            self._privateSay(user, '{}: you should pick {} cards'.format(nick, self.currentBlackCard.pick))
+        cards = []
+        realArgs = []
+        try:
+            for i in args:
+                idx = int(i)
+                if idx > (9+ self.currentBlackCard.pick):
+                    raise ValueError('')
+                realArgs.append(idx - 1) 
+                cards.append(player.heap[idx])
+        except ValueError:
+            self._privateSay(user, 'argument should be number between 1 and {}'.format(9+self.currentBlackCard.pick))
+        player.removeCards(cards)
+        self.playedCards.add(user, cards)
+        self._privateSay(user, 'ok, your turn is in the machine')
+
+    def _endWaitWhiteCard(self):
+        ''' 6) discard lazy people that not played white card '''
+        with self.lockState:
+            if self.state != 'WAIT_WHITE':
+               return
+        logger.info('timeout for playing white card is expired')
+        self._say('timeout for playing white card is expired')
+        self._say("peoples that haven't played are disquilified for this turn")
+        self._beginCzarTurn()    
+
+    def _beginCzarTurn(self):
+        ''' 7) show all proposition and wait for the czar decision '''
+        pass
+
+    def _selectWinner(self, serverData, channel, user, args):
+        ''' 8) select the winner '''
+        pass
+
+    def joinCmd(self, serverData, channel, user, args):
+        with self.lockState:
+            if self.state == 'WAIT_WHITE':
+                #we are on step 5
+                self._playWhiteCards(serverData, channel, user, args)
+            elif self.state == 'WAIT_CZAR':
+                #we are on step 8
+                self._selectWinner(serverData, channel, user, args)
+            else:
+                self.warning('command used in bad context')
+                return
 
     def start(self, nick):
         logger.info('new game on channel ' + self.channel)
