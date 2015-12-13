b4dd48913960fa42950b3797a4f940bd41ac3cc6
Alexis Lameire <alexis.lameire@gmail.com>
2015-12-13 23:23:33 +0100

    add better logging

---
+++ pythonCah/log.py
@@ -0,0 +1,15 @@
+import logging
+import sys
+
+def prepareLogging(loggingLevel):
+    logger = logging.getLogger()
+    logger.setLevel(loggingLevel)
+
+    formatStr = '%(asctime)s [%(levelname)-8s] %(filename)s:%(lineno)s %(message)s'
+    formater = logging.Formatter(formatStr, '%H:%M:%S')
+
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setFormatter(formater)
+    handler.setLevel(loggingLevel)
+   
+    logger.addHandler(handler)   
