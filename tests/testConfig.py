b71c055986531f3552b52ea3a187df9fbe215148
Alexis Lameire <alexis.lameire@gmail.com>
2015-12-26 18:41:13 +0100

    first test implementation

--- tests/testConfig.py
+++
@@ -1,3 +0,0 @@
-from config import loadConfig
-
-loadConfig('config.json')
---
+++ tests/test_config.py
@@ -0,0 +1,10 @@
+from pythonCah.config import loadConfig 
+import pytest
+
+def test_errorJson():
+   with pytest.raises(ValueError):
+       loadConfig('jdt/errorJson.json')
+
+def test_unavailableFile():
+   with pytest.raises(FileNotFoundError):
+       loadConfig('jdt/notAFile.json') 
