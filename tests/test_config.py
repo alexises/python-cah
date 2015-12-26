6dc897123e3e0f4c9af14d603191331f02fe5aa8
Alexis Lameire <alexis.lameire@gmail.com>
2015-12-26 18:46:57 +0100

    solve python style

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,10 +1,12 @@
-from pythonCah.config import loadConfig 
+from pythonCah.config import loadConfig
 import pytest
 
+
 def test_errorJson():
-   with pytest.raises(ValueError):
-       loadConfig('jdt/errorJson.json')
+    with pytest.raises(ValueError):
+        loadConfig('jdt/errorJson.json')
+
 
 def test_unavailableFile():
-   with pytest.raises(FileNotFoundError):
-       loadConfig('jdt/notAFile.json') 
+    with pytest.raises(FileNotFoundError):
+        loadConfig('jdt/notAFile.json')
